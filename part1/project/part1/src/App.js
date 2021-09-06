e96a0c2d2f1656447c062c039cfaf0a4529c2626
pattamite <jittapat007@gmail.com>
2021-09-06 17:34:55 +0700

    Update part1 project

--- part1/project/part1/src/App.js
+++ part1/project/part1/src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+
+// React component names must be capitalized!
+function Hello(props) {
+  
+  return (
+    <p>Hello world {props.name} {props.number}</p>
+  )
+}
 
 function App() {
+  const a = 10;
+  const b = 20;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    // can use <>
+    <div> 
+      <Hello name="Pattamite" number={a + b}/>
+      <p>{a} plus {b} is {a + b}</p>
+    </div> // can use </>
   );
 }
 
