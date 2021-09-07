e089d8cda72b06f116405067624e2f41e442dfcb
pattamite <jittapat007@gmail.com>
2021-09-07 15:03:19 +0700

    Implement assignment unisafe of part1

--- part1/assignment/unisafe/src/App.js
+++ part1/assignment/unisafe/src/App.js
@@ -1,24 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
 
 function App() {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+  
+  function goodButtonHandler() {
+    setGood(good + 1);
+  }
+
+  function neutralButtonHandler() {
+    setNeutral(neutral + 1);
+  }
+
+  function badButtonHandler() {
+    setBad(bad + 1);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header text="Give Feedback" />
+      <Button text="Good" clickHandlerFunction={goodButtonHandler} />
+      <Button text="Neutral" clickHandlerFunction={neutralButtonHandler} />
+      <Button text="Bad" clickHandlerFunction={badButtonHandler} />
+      <Header text="Statistics" />
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
+  );
+}
+
+function Header({text}) {
+  return (
+    <h1>{text}</h1>
+  );
+}
+
+function Button({text, clickHandlerFunction}) {
+  return (
+    <button onClick={clickHandlerFunction}>
+      {text}
+    </button>
+  );
+}
+
+function Statistics({good, neutral, bad}) {
+  const totalFeedback = good + neutral + bad;
+
+  if(totalFeedback === 0){
+    return (
+      <p>No feedback given.</p>
+    );
+  }
+
+  return (
+    <table>
+      <tbody>
+        <StatisticTableRow text="Good" value={good} />
+        <StatisticTableRow text="Neutral" value={neutral} />
+        <StatisticTableRow text="Bad" value={bad} />
+        <StatisticTableRow text="Average" value={(good - bad) / totalFeedback} />
+        <StatisticTableRow text="Positive" value={(good / totalFeedback * 100).toString() + " %"} />
+      </tbody>
+    </table>
+  );
+}
+
+function StatisticTableRow({text, value}) {
+  return (
+    <tr>
+      <td><b>{text}</b></td>
+      <td>{value}</td>
+    </tr>
   );
 }
 
