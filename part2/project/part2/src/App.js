eb16886341ca77697ee589308c8dfaec747112f6
pattamite <jittapat007@gmail.com>
2021-09-07 18:01:25 +0700

    Update project of 2.b

--- part2/project/part2/src/App.js
+++ part2/project/part2/src/App.js
@@ -1,16 +1,57 @@
-import React from 'react'
+import React, {useState} from 'react'
 import Note from './components/Note'
 
 function App({notes}) {
-  //  Each child in a LIST should have a unique "key" prop.
+  const [currentNotes, setNotes] = useState(notes);
+  const [newNote, setNewNote] = useState('A new note...');
+  const [showAll, setShowAll] = useState(true);
+
+  function addNote(event) {
+    event.preventDefault();
+    const newNoteObject = {
+      content: newNote,
+      date: new Date().toISOString(),
+      important: Math.random() < 0.5,
+      id: currentNotes.length + 1,
+    };
+    setNotes(currentNotes.concat(newNoteObject));
+    console.log('button clicked', event.target);
+  }
+
+  function handleNewNoteChange(event) {
+    console.log(event.target.value);
+    setNewNote(event.target.value);
+  }
+
+  function handleShowAllButton() {
+    setShowAll(!showAll);
+  }
+  
+  const notesToShow = showAll
+    ? currentNotes
+    : currentNotes.filter( (note) => {return note.important;} );
+
+  //  Each child in a LIST (not <ul>) should have a unique "key" prop.
   return (
     <div>
       <h1>Notes</h1>
+      <div>
+        <button onClick={handleShowAllButton}>
+          Show {showAll ? 'important' : 'all'}
+        </button>
+      </div>
       <ul>
-        {notes.map( (note) => 
+        {notesToShow.map( (note) => 
         <Note key={note.id} note={note} />
         )}
       </ul>
+      <form onSubmit={addNote}>
+        <input 
+          value={newNote}
+          onChange={handleNewNoteChange}
+        />
+        <button type="submit">Save</button>
+      </form>
     </div>
   );
 }
