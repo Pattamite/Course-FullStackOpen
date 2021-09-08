fcc789f4343c6a88e8439d643eb38e4470ce37e9
pattamite <jittapat007@gmail.com>
2021-09-08 17:50:56 +0700

    Implement assignment persons of part3.a

---
+++ part3/assignment/index.js
@@ -0,0 +1,100 @@
+const express = require('express');
+
+const randomIdRange = 1000000000;
+
+let persons = [
+  { 
+    "id": 1,
+    "name": "Arto Hellas", 
+    "number": "040-123456"
+  },
+  { 
+    "id": 2,
+    "name": "Ada Lovelace", 
+    "number": "39-44-5323523"
+  },
+  { 
+    "id": 3,
+    "name": "Dan Abramov", 
+    "number": "12-43-234345"
+  },
+  { 
+    "id": 4,
+    "name": "Mary Poppendieck", 
+    "number": "39-23-6423122"
+  }
+]
+
+const app = express();
+
+app.use(express.json());
+
+app.get('/info', (request, response) => {
+  const currentDate = new Date();
+  response.send(`
+  <p>Phonebook has info for ${persons.length} people</p>
+  <p>${currentDate}</p>
+  `); 
+});
+
+app.get('/api/persons', (request, response) => {
+  response.json(persons); 
+});
+
+app.get('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id);
+  const person = persons.find((person) => {return person.id === id;});
+
+  if(person) {
+    response.json(person); 
+  } else {
+    response.status(404).end();
+  }
+});
+
+app.delete('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id);
+  persons = persons.filter((person) => {return person.id !== id;});
+
+  response.status(204).end();
+});
+
+app.post('/api/persons', (request, response) => {
+
+  const body = request.body
+
+  
+
+  if(!body.name 
+      || persons.find((person) => {
+        return person.name === body.name
+  }))
+  {
+    return response.status(400).json({
+      error: 'name must be unique'
+    });
+  }
+
+  if(!body.number) {
+    return response.status(400).json({
+      error: 'number is missing'
+    });
+  }
+
+  const person = {
+    id: generateId(),
+    name: body.name,
+    number: body.number,
+  };
+  persons = persons.concat(person);
+  response.json(person);
+});
+
+function generateId() {
+  return Math.floor(Math.random() * randomIdRange);
+}
+
+const PORT = 3001;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
--- part3/project/index.js
+++ part3/project/index.js
@@ -21,9 +21,9 @@ let notes = [
   }
 ]
 
-const app = express()
+const app = express();
 
-app.use(express.json())
+app.use(express.json());
 
 app.get('/', (request, response) => {
   response.send('<h1>Hello World!</h1>'); 
