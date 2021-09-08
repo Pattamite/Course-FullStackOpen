51d37bbe0bf79ca6a68095f89ebbbb89c9a983c6
pattamite <jittapat007@gmail.com>
2021-09-08 18:16:40 +0700

    Update assignment phonebook of part3.a

--- part3/assignment/index.js
+++ part3/assignment/index.js
@@ -1,83 +1,108 @@
-const express = require('express');
+const express = require("express");
+const morgan = require("morgan");
 
 const randomIdRange = 1000000000;
 
 let persons = [
-  { 
-    "id": 1,
-    "name": "Arto Hellas", 
-    "number": "040-123456"
+  {
+    id: 1,
+    name: "Arto Hellas",
+    number: "040-123456",
   },
-  { 
-    "id": 2,
-    "name": "Ada Lovelace", 
-    "number": "39-44-5323523"
+  {
+    id: 2,
+    name: "Ada Lovelace",
+    number: "39-44-5323523",
   },
-  { 
-    "id": 3,
-    "name": "Dan Abramov", 
-    "number": "12-43-234345"
+  {
+    id: 3,
+    name: "Dan Abramov",
+    number: "12-43-234345",
   },
-  { 
-    "id": 4,
-    "name": "Mary Poppendieck", 
-    "number": "39-23-6423122"
+  {
+    id: 4,
+    name: "Mary Poppendieck",
+    number: "39-23-6423122",
+  },
+];
+
+morgan.token("jsonData", (request, response) => {
+  const body = request.body;
+  if (!body) {
+    return "";
   }
-]
+  return JSON.stringify(body);
+});
+
+function morganFormat(tokens, req, res) {
+  console.log(tokens.body);
+  return [
+    tokens.method(req, res),
+    tokens.url(req, res),
+    tokens.status(req, res),
+    tokens.res(req, res, "content-length"),
+    "-",
+    tokens["response-time"](req, res),
+    "ms",
+    tokens.jsonData(req, res),
+  ].join(" ");
+}
 
 const app = express();
-
+app.use(morgan(morganFormat));
 app.use(express.json());
 
-app.get('/info', (request, response) => {
+app.get("/info", (request, response) => {
   const currentDate = new Date();
   response.send(`
   <p>Phonebook has info for ${persons.length} people</p>
   <p>${currentDate}</p>
-  `); 
+  `);
 });
 
-app.get('/api/persons', (request, response) => {
-  response.json(persons); 
+app.get("/api/persons", (request, response) => {
+  response.json(persons);
 });
 
-app.get('/api/persons/:id', (request, response) => {
+app.get("/api/persons/:id", (request, response) => {
   const id = Number(request.params.id);
-  const person = persons.find((person) => {return person.id === id;});
+  const person = persons.find((person) => {
+    return person.id === id;
+  });
 
-  if(person) {
-    response.json(person); 
+  if (person) {
+    response.json(person);
   } else {
     response.status(404).end();
   }
 });
 
-app.delete('/api/persons/:id', (request, response) => {
+app.delete("/api/persons/:id", (request, response) => {
   const id = Number(request.params.id);
-  persons = persons.filter((person) => {return person.id !== id;});
+  persons = persons.filter((person) => {
+    return person.id !== id;
+  });
 
   response.status(204).end();
 });
 
-app.post('/api/persons', (request, response) => {
+app.post("/api/persons", (request, response) => {
+  const body = request.body;
 
-  const body = request.body
-
-  
-
-  if(!body.name 
-      || persons.find((person) => {
-        return person.name === body.name
-  }))
-  {
+  if (
+    !body.name ||
+    persons.find((person) => {
+      return person.name === body.name;
+    })
+  ) {
     return response.status(400).json({
-      error: 'name must be unique'
+      error: "name must be unique",
     });
   }
 
-  if(!body.number) {
+  if (!body.number) {
     return response.status(400).json({
-      error: 'number is missing'
+      error: "number is missing",
     });
   }
 
@@ -93,6 +118,12 @@ app.post('/api/persons', (request, response) => {
 function generateId() {
   return Math.floor(Math.random() * randomIdRange);
 }
+
+function unknownEndpoint(request, response) {
+  response.status(404).send({ error: "unknown endpoint" });
+}
+
+app.use(unknownEndpoint);
 
 const PORT = 3001;
 app.listen(PORT, () => {
